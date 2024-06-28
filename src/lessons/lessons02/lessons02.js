016b4eed9ec26a454f85c8854c5a1acc56570707
Vitalij82201 <152536057+Vitalij82201@users.noreply.github.com>
2024-06-28 14:36:07 +0200

    Add homework02

---
+++ src/lessons/lessons02/lessons02.js
@@ -0,0 +1,14 @@
+import Button from "../../components/Button/Button";
+import "./styles.css";
+
+function lessons02() {
+  return (
+    <div className="lessons02-component">
+      <div>
+        <Button />
+        Lessons02
+      </div>
+    </div>
+  );
+}
+export default lessons02;
